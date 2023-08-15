5cd7c924c90a524d116b1f4a1812b81547059e7e
Alexander Temper <alexandertemper27@gmail.com>
2023-08-15 14:27:21 +0200

    Add CLI

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,20 +1,14 @@
+from argparse import ArgumentParser
 from collections import defaultdict
+from pathlib import Path
 from typing import List
 
+from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 
 def _filter_non_acgt(genes: List[SeqRecord]) -> List[SeqRecord]:
     return [gene for gene in genes if set(str(gene.seq)).issubset("ACGT")]
-
-
-def _clip_lenghts(
-    genes: List[SeqRecord], from_percentile: float, to_percentile: float
-) -> List[SeqRecord]:
-    sorted_by_len = sorted(genes, key=lambda gene: len(gene.seq))
-    from_idx = int(len(genes) * from_percentile)
-    to_idx = int(len(genes) * to_percentile)
-    return sorted_by_len[from_idx:to_idx]
 
 
 def _bin_by_length(
@@ -32,11 +26,29 @@ def preprocess(
     genes: List[SeqRecord],
     bin_width: int,
     min_bin_size: int,
-    from_percentile: float = 0.1,
-    to_percentile: float = 0.9,
 ) -> List[List[SeqRecord]]:
     genes = _filter_non_acgt(genes)
-    genes = _clip_lenghts(genes, from_percentile, to_percentile)
     bins = _bin_by_length(genes, bin_width)
     bins = [bin_ for bin_ in bins if len(bin_) > min_bin_size]
     return bins
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "--in_", type=str, help="Genes in FASTA format to preprocess"
+    )
+    arg_parser.add_argument("--out", type=str, help="Directory to save to")
+    arg_parser.add_argument("--bin-width", type=int, help="Size of bins")
+    arg_parser.add_argument(
+        "--min-bin-size", type=int, help="Minimum number of sequences per bin"
+    )
+    args = arg_parser.parse_args()
+    genes = SeqIO.parse(args.in_, format="fasta")
+    bins = preprocess(list(genes), args.bin_width, args.min_bin_size)
+    for bin_ in bins:
+        SeqIO.write(
+            bin_,
+            Path(args.out) / f"{Path(args.in_).stem}_{len(bin_[0])}.fasta",
+            format="fasta",
+        )
